be8416907f4d6c3d6d01f2f31541d36bdfa90561
tremperb <55967770+tremperb@users.noreply.github.com>
2020-06-01 13:26:19 -0700

    some fixes still problems

--- sentiment.cpp
+++ sentiment.cpp
@@ -16,12 +16,12 @@ vector <vector<string> > convert(vector<string> temp, char* file, string outFile
 /////////////////////////
 //Start Classification
 float check_accuracy(vector<int> myTruths, vector<int> expectedTruths);
-void classification(float probVec[][4], vector<vector <string> > trainingData);
+void classification(vector<vector<float> > training, vector<vector <string> > testingData);
 void printToFile(vector<string> vocab, vector<string> label);
 vector<string> sort_vector(vector<string> data);
 void printToFile(vector<string> vocab, vector<vector <string> > labels, string file);
-float getProbability(string id, vector <string> sentence, vector <vector <string> > trainingData, float probVec[][4], int num);
-void get_prob(vector <vector<string> > trainingData, float probVec[][4]);
+float getProbability(string id, vector <string> currSentence, vector <vector <string> > testingData, vector <vector <float> > train, int idx);
+vector <vector<float> > get_prob(vector <vector<string> > trainingData);
 
 
 int main(int argc, char** argv){
@@ -37,19 +37,16 @@ int main(int argc, char** argv){
     //at this point the words are all held in final_vocab and sending that and the file to read into convert
     vector <vector<string> > trainingData = convert(temp, argv[1], "preprocessed_train.txt");
     //Now we can train the data
-    float probVec[trainingData[0].size()][4];
-    get_prob(trainingData, probVec);
-    /*
-    for(int i = 0; i < 400; i++){
-        cout << probVec[i][0] << endl;
-        cout << probVec[i][1] << endl;
-        cout << probVec[i][2] << endl;
-        cout << probVec[i][3] << endl;
+    vector <vector<float> > probVec;
+    probVec = get_prob(trainingData);
+
+    for(int i=0; i < probVec.size(); i++) {
+        for(int k=0; k < probVec[i].size(); k++) {
+            cout << probVec[i][k] << ",";
+        }
         cout << endl;
     }
-    */
-
-    classification(probVec, trainingData);
+    //classification(final_vocab, trainingData);
 
 
     /*Get Testing Data*/
@@ -64,32 +61,33 @@ int main(int argc, char** argv){
     //at this point the words are all held in final_vocab and sending that and the file to read into convert
     vector <vector<string> > testingData = convert(temp2, argv[2], "preprocessed_test.txt");
 
+    classification(probVec, trainingData);
 
     return 0;
 }
 
 
-void get_prob(vector <vector<string> > trainingData, float probVec[][4]){
-    //int probVec[trainingData[0].size()][4];
+vector <vector<float> > get_prob(vector <vector<string> > trainingData){
+    vector <vector<float> > probVec(trainingData[0].size());
     int temp[trainingData[0].size()][4];
-    for(int i = 0; i < trainingData.size(); i++){
-        for(int y = 0; y < trainingData[i].size(); y++){
+    for(int i = 0; i < trainingData[0].size(); i++){
+        for(int y = 0; y < 4; y++){
             temp[i][y] = 0;
         }
     }
     for(int i = 0; i < trainingData.size(); i++){
         for(int y = 0; y < trainingData[i].size(); y++){
             if(trainingData[i][y] == "0" && trainingData[i][trainingData[i].size() - 1] == "0"){
-                temp[i][0]++;
+                temp[y][0]++;
             }
             else if(trainingData[i][y] == "0" && trainingData[i][trainingData[i].size() - 1] == "1"){
-                temp[i][1]++;
+                temp[y][1]++;
             }
             else if(trainingData[i][y] == "1" && trainingData[i][trainingData[i].size() - 1] == "0"){
-                temp[i][2]++;
+                temp[y][2]++;
             }
             else if(trainingData[i][y] == "1" && trainingData[i][trainingData[i].size() - 1] == "1"){
-                temp[i][3]++;
+                temp[y][3]++;
             }
         }
     }
@@ -105,13 +103,15 @@ void get_prob(vector <vector<string> > trainingData, float probVec[][4]){
     }
     float pGood = ((float)probGood) / ((float)trainingData.size()-1);
     float pBad = 1.0 - pGood;
-    for(int i = 0; i < trainingData[i].size(); i++){
-        probVec[i][0] = ((float)temp[i][0]) / probBad;
-        probVec[i][1] = ((float)temp[i][1]) / probGood;
-        probVec[i][2] = ((float)temp[i][2]) / probBad;
-        probVec[i][3] = ((float)temp[i][3]) / probGood;
+    
+    for(int i = 0; i < trainingData[0].size(); i++){
+        probVec[i].push_back(temp[i][0] / probBad);
+        probVec[i].push_back(temp[i][1] / probGood);
+        probVec[i].push_back(temp[i][2] / probBad);
+        probVec[i].push_back(temp[i][3] / probGood);
     }
-    //return probVec;
+    
+    return probVec;
 
 }
 
@@ -349,7 +349,7 @@ float check_accuracy(vector<int> myTruths, vector<int> expectedTruths) {
     return accuracy;
 }
 
-void classification(float probVec[][4], vector<vector <string> > trainingData) {
+void classification(vector<vector <float> > train, vector<vector <string> > testingData) {
 
     /**
      * data[i] corresponds with labels[i]?
@@ -364,13 +364,13 @@ void classification(float probVec[][4], vector<vector <string> > trainingData) {
      */
     /*Find P=good and P=False*/
     int probGood = 0;
-    for(int i=0; i < trainingData.size(); i++) {
+    /*for(int i=0; i < trainingData.size(); i++) {
         if(trainingData[i][trainingData[i].size()-1] == "1") {
             probGood++;
         }
     }
     float pGood = ((float)probGood) / ((float)trainingData.size()-1);
-    float pBad = 1.0 - pGood;
+    float pBad = 1.0 - pGood;*/
 
 
     /*************************/
@@ -379,9 +379,9 @@ void classification(float probVec[][4], vector<vector <string> > trainingData) {
     float negative = 0.0f;
 
     cout << "Processing..." << endl;
-    for(int i=0; i < trainingData.size(); i++) {
+    for(int i=0; i < testingData.size(); i++) { //get classlabel for each sentence
         //find P(Class=1 | sentence[i] = traindata for each sentence) repeat to get probability of eeach sentence
-        if(getProbability("1", trainingData[i], trainingData, probVec, i) >= getProbability("0", trainingData[i], trainingData, probVec, i)) {
+        if(getProbability("1", testingData[i], testingData, train, i) >= getProbability("0", testingData[i], testingData, train, i)) {
             predictedLabels.push_back("1");
         }
         else { //more likely to be false
@@ -392,7 +392,7 @@ void classification(float probVec[][4], vector<vector <string> > trainingData) {
 
     int correct=0;
     for(int i=0; i < predictedLabels.size(); i++) {
-        if(predictedLabels[i] == trainingData[i][trainingData[i].size()-1]) {
+        if(predictedLabels[i] == testingData[i][testingData[i].size()-1]) {
             correct++;
         }
     }
@@ -408,7 +408,7 @@ void classification(float probVec[][4], vector<vector <string> > trainingData) {
 }
 
 
-float getProbability(string id, vector <string> sentence, vector <vector <string> > trainingData, float probVec[][4], int num) {
+float getProbability(string id, vector <string> sentence, vector <vector <string> > trainingData, vector <vector <float> > probVec, int num) {
 
     //P(Class=id | data[0]=labels[0], data[1]=labels[1] and so on)
 /*
@@ -442,13 +442,13 @@ float getProbability(string id, vector <string> sentence, vector <vector <string
 
     probability += log10(((classCheck) / (trainingData.size()+1)));
 */
-    for(int i = 0; i < trainingData.size();){
+    /*for(int i = 0; i < trainingData.size();){
         for(int y = 0; y < 4; y++){
             if(probVec[i][y] > 1.0){
                 probVec[i][y] = 1.0;
             }
         }
-    }
+    }*/
 
     float prob = 1.0;
     float temp[trainingData[0].size()];
@@ -461,26 +461,51 @@ float getProbability(string id, vector <string> sentence, vector <vector <string
     }
     float pGood = ((float)probGood) / ((float)trainingData.size()-1);
     float pBad = 1.0 - pGood;
-
+    float wordMatch = 0.0, classMatch = 0.0, idMatch = 0.0;
+    for(int i = 0; i < trainingData.size(); i++) {
+        for(int k=0; k < trainingData[i].size(); k++) {
+            if(trainingData[num][k] == trainingData[i][k]) {
+                probVec[k][2]++;
+                probVec[k][3]++;
+                wordMatch++;
+                if(trainingData[num][trainingData[num].size()-1] == trainingData[i][trainingData[i].size()-1]) {
+                    classMatch++;
+                    if(id == "1") {
+                        probVec[k][3] += 2;
+                    }
+                    else {
+                        probVec[k][1] += 2;
+                    }
+                }
+            }
+        }
+        if(id == trainingData[i][trainingData[i].size()-1]) {
+            idMatch++;
+        }
+        prob += log10((wordMatch+1) / (classMatch+2));
+    }
+    prob += log10(((idMatch) / (trainingData.size()+1)));
     //for(int i = 0; i < trainingData.size(); i++){
         for(int y = 0; y < trainingData[num].size(); y++){
-            if(trainingData[num][y] == "0" && id == "0"){
+            if(trainingData[num][y] == "0" && id == "0") {
                 temp[y] = probVec[y][0];
             }
-            else if(trainingData[num][y] == "0" && id == "1"){
+            else if(trainingData[num][y] == "0" && id == "1") {
                 temp[y] = probVec[y][1];
             }
-            else if(trainingData[num][y] == "1" && id == "0"){
+            else if(trainingData[num][y] == "1" && id == "0") {
                 temp[y] = probVec[y][2];
             }
-            else if(trainingData[num][y] == "1" && id == "1"){
+            else if(trainingData[num][y] == "1" && id == "1") {
                 temp[y] = probVec[y][3];
             }
         }
     //}
-    prob = temp[0];
+    cout << prob << endl;
+    prob += temp[0];
     for(int i = 1; i < trainingData[0].size(); i++){
         prob = prob * temp[i];
+        cout << prob << endl;
     }
     if(id == "0"){
         prob = prob * pBad;
@@ -488,15 +513,6 @@ float getProbability(string id, vector <string> sentence, vector <vector <string
     else if(id == "1"){
         prob = prob * pGood;
     }
-
+    
     return prob;
 }
-
-
-
-
-
-
-
-
-
